5c903c2082e28e40320a8ed9b057badb9e995238
Saipranav Janyavula <saipranav.janyavula@frogforce503.org>
2021-05-14 17:29:16 -0400

    setup db connection

---
+++ scrapers/streaming.js
@@ -0,0 +1,28 @@
+const puppeteer = require("puppeteer");
+const mysql = require("mysql");
+var connection = mysql.createConnection({
+    host: process.env.DBHOST,
+    user: process.env.DBUSER,
+    password: process.env.DBPASSWORD,
+    database: "movieFinder"
+});
+
+connection.connect();
+
+const movieCount = 100;
+const maxYear = 2021;
+const minYear = 1990;
+
+async function getStreamingLocations() {
+    connection.query('select * from choiceTypes', (err, result, field) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log(result[2].choiceName);
+        }
+    });
+};
+
+getStreamingLocations();
+
+connection.end();
