ab633ba19360d6a725508a9bc2fcb692373c877e
Saipranav Janyavula <saipranav.janyavula@frogforce503.org>
2021-05-23 10:00:47 -0400

    Add Arsha Template from bootstrap made

--- app.js
+++ app.js
@@ -4,8 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+//var indexRouter = require('./routes/index');
+//var usersRouter = require('./routes/users');
 
 var app = express();
 
@@ -28,7 +28,7 @@ app.get("/", (req, res) => {
 })
 
 app.get("/index", (req, res) => {
-  res.render("index", {title: "Express"});
+  res.render("index", {title: "MovieFinder.us", name: "Node.js"});
 });
 
 // catch 404 and forward to error handler
